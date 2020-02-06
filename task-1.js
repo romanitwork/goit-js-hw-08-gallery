d7ced589703bc024bf89c30bd2cafc081450c0e0
romanitwork <workromanmail@gmail.com>
2020-02-06 08:54:42 +0200

    prettier

--- task-1.js
+++ task-1.js
@@ -32,15 +32,16 @@ list.appendChild(fragment);
 
 list.addEventListener("click", onCLick);
 function onCLick(e) {
-  if (e.target.classList.contains('gallery__image')) {
-  lightbox.classList.add("is-open");
-  document.querySelector(".lightbox__image").src = e.target.dataset.source;
-}
+  console.log(e);
+  if (e.target.classList.contains("gallery__image")) {
+    lightbox.classList.add("is-open");
+    document.querySelector(".lightbox__image").src = e.target.dataset.source;
+  }
 }
 button.addEventListener("click", buttonClose);
 function buttonClose(e) {
-  if (!e.target.classList.contains('lightbox__image')) {
-  lightbox.classList.remove("is-open");
-  lightbox.removeEventListener('click', buttonClose);
-}
+  if (!e.target.classList.contains("lightbox__image")) {
+    lightbox.classList.remove("is-open");
+    lightbox.removeEventListener("click", buttonClose);
+  }
 }
